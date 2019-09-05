954b62ad9030c1a68b376aa50c84e8c387a05298
Aldo Souza <asjsouzajr@hotmail.com>
2019-09-05 21:16:02 +0200

    tentando realocar no Git em outro repositório

--- src/Principal.java
+++ src/Principal.java
@@ -6,11 +6,10 @@ public class Principal   {
 		Livro l1 = new Livro("A volta dos que nao foram", "Joao sem Letras", 128);
 		l1.setLeitor(p1);
 		l1.abrir();
-		l1.folhear(3);
-		//l1.detalhes();
+		l1.folhear(45);
+		l1.detalhes();
 		l1.getPagAtual();
 		l1.avancarPag();
 		l1.getPagAtual();
-		l1.fechar();
 	}
 }
