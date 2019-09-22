c58765177aecca7d7091b8f1df36b32d9489b1b6
Aldo Souza <asjsouzajr@hotmail.com>
2019-09-22 14:51:24 +0200

    Adding functions for CL Carro in project RlSystem

--- src/rlSystem_Carro/Carro.java
+++ src/rlSystem_Carro/Carro.java
@@ -1,5 +1,91 @@
 package rlSystem_Carro;
 
 public class Carro {
+	public String marca;
+	public String modelo;
+	public int ano;
+	private int marcha = 0;
+	public boolean ligado;
+	public int velocidade;
+	public boolean freioDeMao = true;
 
+	public void frear() {
+		if (this.ligado == true && this.freioDeMao == false && this.velocidade > 0) {
+			this.velocidade -= 20;
+			System.out
+					.println("O carro " + this.modelo + " foi freiado e nova velocidade é " + this.velocidade + "km/h");
+		} else if (this.ligado == true && this.freioDeMao == true) {
+			System.out.println("O carro está com o freio de mao puxado!");
+			System.out.println("Atual velocidade: " + this.velocidade + "km/h");
+		} else if (this.ligado == false && this.freioDeMao == true) {
+			System.out.println("O carro já está parado,desligado e com freio de mao puxado!");
+		}
+	}
+
+	public void soltarFreioDeMao() {
+		this.freioDeMao = false;
+	}
+
+	public void acionarFreioDeMao() {
+		this.freioDeMao = true;
+	}
+
+	public void ligar() {
+		if (this.ligado == false) {
+			this.ligado = true;
+			System.out.println("O " + this.modelo + " foi Ligado");
+		} else {
+			System.out.println("O " + this.modelo + " já está ligado");
+		}
+	}
+
+	public void desligar() {
+		if (this.ligado == true) {
+			if(this.velocidade <= 0) {
+					System.out.println("Carro desligado!");
+				} else {
+					System.out.println("Carro ainda em movimento, pare o carro primeiro!");
+				}
+			} else {
+				System.out.println("O carro já está desligado");
+			}
+				
+			
+	}
+
+	public void acelerar(int speed) {
+		if (this.ligado == true) {
+			if (freioDeMao == false) {
+
+				if (this.marcha == 0) {
+					System.out.println("O carro está ligado, freio de mao está solto, mas o carro está em ponto morto");
+
+				} else if (this.marcha == 1) {
+					if (speed < 30) {
+						this.velocidade = speed;
+						System.out.println("O " + this.modelo + " foi acelerado à " + this.velocidade + " km/h");
+					}
+				} else if (this.marcha == 2) {
+					if (speed < 50) {
+						this.velocidade = speed;
+						System.out.println("O " + this.modelo + " foi acelerado à " + this.velocidade + " km/h");
+					}
+				} else if (this.marcha == 3) {
+					if (speed < 80) {
+						this.velocidade = speed;
+						System.out.println("O " + this.modelo + " foi acelerado à " + this.velocidade + " km/h");
+					}
+				} else if (this.marcha == 4) {
+					if (speed < 100) {
+						this.velocidade = speed;
+						System.out.println("O " + this.modelo + " foi acelerado à " + this.velocidade + " km/h");
+					}
+				}
+			}
+		}
+	}
+
+	public void passarMarcha() {
+		this.marcha ++;
+	}
 }
--- src/rlSystem_Carro/main.java
+++ src/rlSystem_Carro/main.java
@@ -2,9 +2,36 @@ package rlSystem_Carro;
 
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main (String[] args) {
+		Carro carro1 = new Carro();
+		
+		Carro carro2 = new Carro();
+		
+		carro2.marca = "Hyundai";
+		carro2.modelo = "iX35";
+		carro2.ano = 2017;
+		carro2.ligar();
+		carro2.soltarFreioDeMao();
+		
+		carro2.passarMarcha();
+		carro2.acelerar(20);
+		
+		carro2.passarMarcha();
+		carro2.acelerar(40);
+		
+		carro2.passarMarcha();
+		carro2.acelerar(60);
 
+		carro2.passarMarcha();
+		carro2.acelerar(90);
+		
+        carro2.frear();
+		carro2.desligar();
+		carro2.frear();
+		carro2.frear();
+		carro2.frear();
+		carro2.frear();
+		
+		carro2.desligar();
 	}
-
 }
