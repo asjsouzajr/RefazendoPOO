70fd499be51d82bda08208ff6e121e2e8a26572f
Aldo Souza <asjsouzajr@hotmail.com>
2019-09-23 17:18:05 +0200

    Adding Project Livraria do Guanabara

---
+++ src/livraria/Principal.java
@@ -0,0 +1,16 @@
+package livraria;
+
+public class Principal   {
+	public static void main(String[] args) {
+
+		Pessoa p1 = new Pessoa("Pedro Cavalcanti", 44, "Masc");
+		Livro l1 = new Livro("A volta dos que nao foram", "Joao sem Letras", 128);
+		l1.setLeitor(p1);
+		l1.abrir();
+		l1.folhear(105);
+		l1.detalhes();
+		l1.getPagAtual();
+		l1.avancarPag();
+		l1.getPagAtual();
+	}
+}
