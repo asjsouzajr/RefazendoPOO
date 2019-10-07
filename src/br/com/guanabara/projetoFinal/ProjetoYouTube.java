71a40566c68d60b7f6f99439b38b0a381275d794
Aldo Souza <asjsouzajr@hotmail.com>
2019-10-07 18:12:53 +0200

    new testing

--- src/br/com/guanabara/projetoFinal/ProjetoYouTube.java
+++ src/br/com/guanabara/projetoFinal/ProjetoYouTube.java
@@ -18,7 +18,7 @@ public class ProjetoYouTube {
 		System.out.println("..............");
 		Visualizacao vis2 = new Visualizacao(g[0], v[2]);
 		vis2.avaliar();
-		vis2.avaliar(10);
+		vis2.avaliar(5);
 		System.out.println(vis2.toString());	
 		
 		/*
